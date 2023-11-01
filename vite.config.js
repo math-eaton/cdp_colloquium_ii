39e4ddfc92a88b123ae3188a02bf4dc5b5ac9b3e
math-eaton <mheaton@ciesin.columbia.edu>
2023-11-01 00:32:51 -0400

    vite config ii

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/3D_mapping/' // Replace with your GitHub repo name
-})
+    base: process.env.NODE_ENV === 'production' ? '/3D_mapping/' : '/',
+  })
