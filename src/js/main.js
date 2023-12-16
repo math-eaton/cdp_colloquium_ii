040209763197ce4d1fce988216f31cf41a165a95
math-eaton <mheaton@ciesin.columbia.edu>
2023-12-15 23:06:48 -0500

    pin fullscreen to bottom + header proper

--- src/js/main.js
+++ src/js/main.js
@@ -32,61 +32,6 @@ let textColumn = document.getElementById('textColumn');
 let isScrolling = false;
 
 
-// fn to scroll both columns together?
-// imageColumn.addEventListener('scroll', function() {
-//     if (!isScrolling) {
-//         isScrolling = true;
-//         let scrollPercentage = this.scrollTop / this.scrollHeight;
-//         textColumn.scrollTop = scrollPercentage * textColumn.scrollHeight;
-//         setTimeout(() => { isScrolling = false; }, 100); // delay to prevent continuous loop
-//     }
-// });
-
-// textColumn.addEventListener('scroll', function() {
-//     if (!isScrolling) {
-//         isScrolling = true;
-//         let scrollPercentage = this.scrollTop / this.scrollHeight;
-//         imageColumn.scrollTop = scrollPercentage * imageColumn.scrollHeight;
-//         setTimeout(() => { isScrolling = false; }, 100); // delay to prevent continuous loop
-//     }
-// });
-
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     window.addEventListener('scroll', function() {
-//         const scrollPosition = window.scrollY;
-//         const totalHeight = document.body.scrollHeight - window.innerHeight;
-//         const scrollPercentage = (scrollPosition / totalHeight) * 100;
-
-//         const threeContainer = document.getElementById('three-container');
-
-//         if (scrollPercentage > 80) {
-//             // Remove .background class to enable interaction
-//             threeContainer.classList.remove('background');
-//         } else {
-//             // Add .background class back if it's not there
-//             if (!threeContainer.classList.contains('background')) {
-//                 threeContainer.classList.add('background');
-//             }
-//         }
-//     });
-
-//         // Select the 'app' and 'p5Container' elements
-//         const appDiv = document.getElementById('app');
-//         const p5ContainerDiv = document.getElementById('p5Container');
-    
-//         // Add a click event listener to the 'app' div
-//         appDiv.addEventListener('click', function() {
-//             // Trigger fade-out effect on 'p5Container'
-//             console.log("tRIrgg")
-//             p5ContainerDiv.style.transition = 'opacity 0.5s ease-out'; // Smooth transition for the fade-out effect
-//             p5ContainerDiv.style.opacity = '0'; // Set opacity to 0 to fade out
-    
-//             // Optional: Remove the div or make it non-interactive after fade-out
-//             setTimeout(() => {
-//                 p5ContainerDiv.style.display = 'none'; // Hide the div after the fade-out completes
-//             }, 500); // The timeout duration should match the transition duration
-//         });
-// });
 
 document.getElementById('fullscreenButton').addEventListener('click', function() {
     const button = this;
